b934642953824c184e26bbf364abe1994e5d661f
Douglasmantovani <almagedomchaotic19@gmail.com>
2021-01-17 14:53:54 -0300

    Atualização

--- front-end/src/Pages/CadastrarColaborador/index.js
+++ front-end/src/Pages/CadastrarColaborador/index.js
@@ -1,86 +1,17 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
 import Footer from "../../Components/Footer";
 import Header from "../../Components/Header";
+
+import "./style.css";
 
 import Delete from "../../assets/images/cancel.webp";
 import Input from "../../Components/Input";
 import AccessBar from "../../Components/AccessBar";
 import BlackButton from "../../Components/BlackButton";
 import AccessMenu from "../../Components/AccessMenu";
-
-import { uri } from "../../services/conexao";
-
-import "./style.css";
+import user from '../../assets/images/user.webp'
 
 export default function CadastrarColaborador() {
-  const [Colaboradores, setColaboradores] = useState([]);
-  const [Email, SetEmail] = useState("");
-  const [Senha, SetSenha] = useState("");
-  const [ConfirmarSenha, SetConfirmarSenha] = useState("");
-
-  useEffect(() => {
-    listarColaboradores();
-  }, []);
-
-  function salvar() {
-    const data = {
-      Email: Email,
-      Senha: Senha,
-      perguntaSeguranca: "Em que ano foi fundado a Microsoft",
-      respostaSeguranca: "No ano de fundação",
-    };
-
-    if (Senha !== ConfirmarSenha || Senha === "" || ConfirmarSenha === "") {
-      alert("As senhas não coincidem, ou não foram preenchidas.");
-    } else {
-      fetch(`${uri}/api/Administrador/AdicionarColaborador`, {
-        body: JSON.stringify(data),
-        method: "POST",
-          headers: {
-            "content-type": "application/json",
-            authorization: "Bearer " + localStorage.getItem("token"),
-          }
-        })
-        .then((response) => response.json())
-        .then((dados) => {
-          alert("Colaborador cadastrado com sucesso");
-          listarColaboradores();
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-  }
-
-  const listarColaboradores = () => {
-    fetch(`${uri}/api/Administrador/ListarColaboradores`, {
-      method: "GET",
-      headers: {
-        authorization: "Bearer " + localStorage.getItem("token"),
-      },
-    })
-      .then((response) => response.json())
-      .then((dados) => {
-        setColaboradores(dados);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const DeletarAdm = (idAdm) => {
-    fetch(`${uri}/api/Administrador/DeletarAdminstrador/${idAdm}`, {
-      method: "DELETE",
-      headers: {
-        authorization: "Bearer " + localStorage.getItem("token"),
-      },
-    })
-      .then((response) => response.json())
-      .then((dados) => {
-        alert(dados);
-        listarColaboradores();
-      })
-      .catch((err) => console.error(err));
-  };
 
   return (
     <div className="corpo">
@@ -90,37 +21,47 @@ export default function CadastrarColaborador() {
       <div className="margin">
         <div className="perfis">
           <h1 className="colaboradores">Colaboradores:</h1>
-          {Colaboradores.map((item) => {
-            return (
               <div className="perfilBox">
                 <div className="horizontal">
                   <div className="perfil">
-                    <img
-                      className="user"
-                      src={`${uri}/imgPerfil/${item.caminhoImagem}`}
-                      alt="Iamgem de perfil do administrador"
-                    />
-                    <h4>Colaborador</h4>
+                    <img className="user" src={user} alt="Iamgem de perfil do administrador" />
+                    <h4>Administrador</h4>
                   </div>
                   <div className="infos">
-                    <p>ID: {item.idUsuario}</p>
-                    <p>E-mail: {item.email}</p>
+                    <p>ID:1</p>
+                    <p>E-mail:administrador@gmail.com</p>
                   </div>
                 </div>
-
                 <div className="ban">
                   <div className="banir">
                     <p>Deletar</p>
                     <img
                       src={Delete}
-                      onClick={() => DeletarAdm(item.idUsuario)}
-                      alt="Apagar"
                     />
                   </div>
                 </div>
               </div>
-            );
-          })}
+
+              <div className="perfilBox">
+                <div className="horizontal">
+                  <div className="perfil">
+                    <img className="user" src={user} alt="Iamgem de perfil do administrador" />
+                    <h4>Colaborador</h4>
+                  </div>
+                  <div className="infos">
+                    <p>ID:2</p>
+                    <p>E-mail:Colaborador@gmail.com</p>
+                  </div>
+                </div>
+                <div className="ban">
+                  <div className="banir">
+                    <p>Deletar</p>
+                    <img
+                      src={Delete}
+                    />
+                  </div>
+                </div>
+              </div>
         </div>
         <hr className="barraVertical" />
         <div className="cadastrarBox">
@@ -132,12 +73,8 @@ export default function CadastrarColaborador() {
             </p>
           </div>
           <div className="camposCadastro">
-            <form onSubmit={event => {
-                    event.preventDefault();
-                    salvar();
-                  }}>
             <Input
-              id="emailColab"
+            id="emailColab"
               className="div-select"
               name="emailColab"
               label="*E-mail"
@@ -146,11 +83,10 @@ export default function CadastrarColaborador() {
               maxLength={254}
               minLength={5}
               required
-              onChange={(e) => SetEmail(e.target.value)}
             />
 
             <Input
-              id="senhaColab"
+            id="senhaColab"
               className="div-select"
               name="senhaColab"
               label="*Senha"
@@ -159,11 +95,10 @@ export default function CadastrarColaborador() {
               maxLength={15}
               minLength={9}
               required
-              onChange={(e) => SetSenha(e.target.value)}
             />
 
             <Input
-              id="confirmarSenhaColab"
+            id="confirmarSenhaColab"
               className="div-select"
               name="confirmarSenhaColab"
               label="*Confirmar Senha"
@@ -172,14 +107,12 @@ export default function CadastrarColaborador() {
               maxLength={15}
               minLength={9}
               required
-              onChange={(e) => SetConfirmarSenha(e.target.value)}
             />
+          </div>
           <div className="botaoCadastrar">
-            <BlackButton type="submit" name="Cadastrar" onClick={salvar}>
+            <BlackButton type="submit" name="Cadastrar">
               Cadastrar
             </BlackButton>
-          </div>
-            </form>
           </div>
         </div>
       </div>
